596cf6ae834174827ab0267ec17235f9da2ee5b8
QiuZiyi <qiuzy21@mails.tsinghua.edu.cn>
2025-10-09 17:15:05 +0800

    debug test

--- vllm/v1/spec_decode/history_rollout.py
+++ vllm/v1/spec_decode/history_rollout.py
@@ -12,6 +12,7 @@ class HistoryRolloutProposer:
         # Maximum length of the HistoryRolloutTree to match.
         self.max_n = vllm_config.speculative_config.prompt_lookup_max
         # self.k = vllm_config.speculative_config.num_speculative_tokens
+        self.debug = True
 
     def propose(
         self,
@@ -19,32 +20,14 @@ class HistoryRolloutProposer:
         sampled_token_ids: list[int],
         prompt_token_ids: list[int]
     ) -> Optional[np.ndarray]:
-        """Proposes the next sequence of tokens based on n-gram pattern 
-        matching in the context. The function finds matches of the last n 
-        tokens in the previous context, and returns k tokens that followed 
-        that match.
-        
-        Args:
-            context_token_ids: Numpy array of token IDs representing the 
-                               context sequence.
-
-        Returns:
-            np.ndarray: The sequence of tokens that followed 
-                        the matched n-gram in the context.
-            None: If no matching n-gram pattern is found.
-
-        Example:
-            If context_token_ids = [1,2,3,4,2,3], min_n = 2, max_n = 3, and
-            k = 4:
-            - The last 3 (= max_n) tokens [4,2,3] cannot find a match.
-            - The last 2 tokens [2,3] will be matched against the previous 
-              4 tokens [1,2,3,4].
-            - Finding a match of [2,3] would return the tokens that 
-              followed that pattern. Here we will return [4,2,3] because 
-              we only have three tokens after the match.
+        """Proposes the next sequence of tokens based on history rollout
+        speculative decoding pattern.
         """
         prompt_id = str(hash(tuple(prompt_token_ids)))
-        history_trees = GlobalRewardAwareSuffixTreeGroup() 
+        if self.debug:
+            print(f"prompt_token_ids={prompt_token_ids}")
+            print(f"prompt_id={prompt_id}")
+        history_trees = GlobalRewardAwareSuffixTreeGroup()
         if not history_trees.exist(prompt_id):
             return []
         else:
