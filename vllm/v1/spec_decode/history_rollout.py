b1ea0a1a02d22a8483409420a362a0c18866b9ba
QiuZiyi <qiuzy21@mails.tsinghua.edu.cn>
2025-10-23 16:02:29 +0800

    fix bug

--- vllm/v1/spec_decode/history_rollout.py
+++ vllm/v1/spec_decode/history_rollout.py
@@ -59,11 +59,12 @@ class HistoryRolloutProposer:
         for i in range(batch_size):
             if not ray.get(exist_tasks[i]):
                 predict_tasks.append(None)
-            sampled_token_ids = sampled_token_id_list[i]
-            prompt_id = str(hash(tuple(prompt_token_id_list[i])))
-            if len(sampled_token_ids) >= self.min_n:
-                prefix = sampled_token_ids[-self.min_n:]
-                predict_tasks.append(self.history_trees.predict(prompt_id, prefix, accept_length_list[i]))
+            else:
+                sampled_token_ids = sampled_token_id_list[i]
+                prompt_id = str(hash(tuple(prompt_token_id_list[i])))
+                if len(sampled_token_ids) >= self.min_n:
+                    prefix = sampled_token_ids[-self.min_n:]
+                    predict_tasks.append(self.history_trees.predict(prompt_id, prefix, accept_length_list[i]))
 
         batch_draft_tokens = []
         for i in range(batch_size):
